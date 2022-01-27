840f73d6db06c42792f53231a3b3c3c56388e7df
Ivan Ismail <ivanismail2@gmail.com>
2022-01-27 16:23:24 +0700

    fix: routing

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,3 +1,5 @@
+import 'package:crud_contact/app/modules/contact/views/contact_add_view.dart';
+import 'package:crud_contact/app/modules/contact/views/contact_detail_view.dart';
 import 'package:get/get.dart';
 
 import 'package:crud_contact/app/modules/contact/bindings/contact_binding.dart';
@@ -14,6 +16,16 @@ class AppPages {
     GetPage(
       name: _Paths.CONTACT,
       page: () => ContactView(),
+      binding: ContactBinding(),
+    ),
+    GetPage(
+      name: _Paths.CONTACT_DETAIL,
+      page: () => ContactDetailView(),
+      binding: ContactBinding(),
+    ),
+    GetPage(
+      name: _Paths.CONTACT_ADD,
+      page: () => ContactAddView(),
       binding: ContactBinding(),
     ),
   ];
